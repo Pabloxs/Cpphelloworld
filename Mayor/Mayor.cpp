9c573d47d800099d5287f57fd9935e9427de59b9
Pabloxs <orellana_pablo_n@hotmail.com>
2017-04-10 20:54:38 -0300

    Add files via upload

---
+++ Mayor/Mayor.cpp
@@ -0,0 +1,29 @@
+/*Mayor
+PabloOrellana
+10042017
+*/
+
+
+#include <iostream>
+using namespace std;
+
+main(){
+	int a;
+	int b;
+	cin>>a;
+	cin>>b;
+	if (a>b){
+		cout<<a<<" ES MAYOR QUE "<<b<<endl;
+			}
+	else
+		if(a<b){
+			cout<<b<<" ES MAYOR QUE "<<a<<endl;
+		}
+		else{
+			cout<<a<<" ES IGUAL QUE "<<b<<endl;
+		}
+		  
+	return 0;
+} 
+
+
